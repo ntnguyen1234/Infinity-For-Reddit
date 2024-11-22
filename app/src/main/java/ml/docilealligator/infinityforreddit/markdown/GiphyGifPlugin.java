dd312936878c30da00a6425f07b99ea677edba2e
ntnguyen1234 <thanhnguyen.nguyen94@gmail.com>
2024-11-22 07:09:32 +0700

    commenting giphy related

--- app/src/main/java/ml/docilealligator/infinityforreddit/markdown/GiphyGifPlugin.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/markdown/GiphyGifPlugin.java
@@ -1,31 +1,31 @@
-package ml.docilealligator.infinityforreddit.markdown;
+// package ml.docilealligator.infinityforreddit.markdown;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+// import androidx.annotation.NonNull;
+// import androidx.annotation.Nullable;
 
-import org.commonmark.parser.Parser;
+// import org.commonmark.parser.Parser;
 
-import java.util.List;
+// import java.util.List;
 
-import io.noties.markwon.AbstractMarkwonPlugin;
-import ml.docilealligator.infinityforreddit.thing.GiphyGif;
-import ml.docilealligator.infinityforreddit.thing.UploadedImage;
+// import io.noties.markwon.AbstractMarkwonPlugin;
+// import ml.docilealligator.infinityforreddit.thing.GiphyGif;
+// import ml.docilealligator.infinityforreddit.thing.UploadedImage;
 
-public class GiphyGifPlugin extends AbstractMarkwonPlugin {
-    private final GiphyGifBlockParser.Factory factory;
+// public class GiphyGifPlugin extends AbstractMarkwonPlugin {
+//     private final GiphyGifBlockParser.Factory factory;
 
-    public GiphyGifPlugin(@Nullable GiphyGif giphyGif, @Nullable List<UploadedImage> uploadedImages) {
-        this.factory = new GiphyGifBlockParser.Factory(giphyGif, uploadedImages);
-    }
+//     public GiphyGifPlugin(@Nullable GiphyGif giphyGif, @Nullable List<UploadedImage> uploadedImages) {
+//         this.factory = new GiphyGifBlockParser.Factory(giphyGif, uploadedImages);
+//     }
 
-    @NonNull
-    @Override
-    public String processMarkdown(@NonNull String markdown) {
-        return super.processMarkdown(markdown);
-    }
+//     @NonNull
+//     @Override
+//     public String processMarkdown(@NonNull String markdown) {
+//         return super.processMarkdown(markdown);
+//     }
 
-    @Override
-    public void configureParser(@NonNull Parser.Builder builder) {
-        builder.customBlockParserFactory(factory);
-    }
-}
+//     @Override
+//     public void configureParser(@NonNull Parser.Builder builder) {
+//         builder.customBlockParserFactory(factory);
+//     }
+// }
